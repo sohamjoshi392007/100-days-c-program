fc139ab9c9406362a6aaacd702583064f62bac43
Soham Joshi <sohamjoshi32007@gmail.com>
2026-09-08 23:14:39 +0530

    Day 14 Question 28

---
+++ day14Q28.c
@@ -0,0 +1,20 @@
+//Q28: Write a program to print the product of even numbers from 1 to n.
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long product = 1;
+    
+   
+    scanf("%d", &n);
+    
+   
+    for (int i = 2; i <= n; i += 2) {
+        product *= i;
+    }
+    
+  
+    printf("%lld\n", product);
+    
+    return 0;
+}
