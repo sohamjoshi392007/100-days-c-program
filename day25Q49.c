4e2fcce81467d6e57f56805a5de29278bb04b830
Soham <sohamjoshi32007@gmail.com>
2026-09-09 15:34:07 +0530

    Day 25 Question 49

---
+++ day25Q49.c
@@ -0,0 +1,23 @@
+// Q49: Write a program to print the following pattern:
+/* 5
+  45
+  345
+  2345
+  12345 */
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+    
+   
+    for (int i = 1; i <= rows; i++) {
+      
+        for (int j = 6 - i; j <= rows; j++) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+    
+    return 0;
+}
