d436a70874db3fcc63cf0152c3da644ca3391f93
Soham <sohamjoshi32007@gmail.com>
2026-09-09 15:06:51 +0530

    Day 20 Question 40

---
+++ day20Q40.c
@@ -0,0 +1,23 @@
+//Q40: Write a program to find the 1’s complement of a binary number and print it.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char binary[100];
+    
+   
+    scanf("%s", binary);
+
+    for (int i = 0; binary[i] != '\0'; i++) {
+        if (binary[i] == '0') {
+            binary[i] = '1';
+        } else if (binary[i] == '1') {
+            binary[i] = '0';
+        }
+    }
+    
+    
+    printf("%s\n", binary);
+    
+    return 0;
+}
