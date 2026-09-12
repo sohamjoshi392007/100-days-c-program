1fc6b7d9a60811d518a7a99b95218f0233929d9b
Soham Joshi <sohamjoshi32007@gmail.com>
2026-09-12 23:51:30 +0530

    day2Q3.c

--- day2Q3.c
+++ day2Q3.c
@@ -4,14 +4,14 @@
 int main() {
     int length, breadth;
     
-    // Read length and breadth from input
+
     scanf("%d %d", &length, &breadth);
     
-    // Calculate area and perimeter
+
     int area = length * breadth;
     int perimeter = 2 * (length + breadth);
     
-    // Display the results in the exact requested format
+
     printf("Area=%d, Perimeter=%d\n", area, perimeter);
     
     return 0;
