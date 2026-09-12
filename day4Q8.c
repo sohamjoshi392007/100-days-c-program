7667bb69b9293b9d78b3557bbf13aea05d9340c0
Soham Joshi <sohamjoshi32007@gmail.com>
2026-09-12 23:54:42 +0530

    day4Q8.c

--- day4Q8.c
+++ day4Q8.c
@@ -4,13 +4,13 @@
 int main() {
     int n;
     
-    // Read the value of n from input
+
     scanf("%d", &n);
     
-    // Calculate the sum of first n natural numbers using the formula: n * (n + 1) / 2
+
     int sum = n * (n + 1) / 2;
     
-    // Display the result in the exact requested format
+
     printf("Sum=%d\n", sum);
     
     return 0;
