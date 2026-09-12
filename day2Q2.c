89e4b14a8a766d74dd790d555a6b62efb19f3689
Soham Joshi <sohamjoshi32007@gmail.com>
2026-09-12 23:51:03 +0530

    day2Q2.c

--- day2Q2.c
+++ day2Q2.c
@@ -4,16 +4,15 @@
 int main() {
     int a, b;
     
-    // Read two integers from input
+
     scanf("%d %d", &a, &b);
     
-    // Calculate the operations
+
     int sum = a + b;
     int diff = a - b;
     int product = a * b;
-    int quotient = a / b; // Integer division
-    
-    // Display the results in the exact requested format
+    int quotient = a / b; 
+
     printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d\n", sum, diff, product, quotient);
     
     return 0;
