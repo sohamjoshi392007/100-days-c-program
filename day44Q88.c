79e8fda8759a19df5bc38bc07a8a8043031b49b2
Soham <sohamjoshi32007@gmail.com>
2026-09-22 17:38:22 +0530

    Day 44 Question 88

---
+++ day44Q88.c
@@ -0,0 +1,29 @@
+//Q88: Replace spaces with hyphens in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+hello world
+Output 1:
+hello-world
+
+*/
+#include <stdio.h>
+
+int main() {
+    char str[200];
+
+
+    scanf("%[^\n]", str);
+
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';
+        }
+    }
+
+    printf("%s\n", str);
+
+    return 0;
+}
